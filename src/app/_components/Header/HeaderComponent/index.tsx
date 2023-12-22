d90641c9a58d7f9ec6ee25cca49d8324505f4753
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-12-22 23:48:59 +0600

    added navigation and mobile nav

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,16 +1,33 @@
+'use client'
+
 import React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constant'
 import { Gutter } from '../../Gutter'
+import MobileNav from '../MobileNav'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
   return (
-    <nav>
-      <Gutter>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
         <Link href="/">
-          <img src="/logo-black.svg" alt="logo" />
+          <Image width={170} height={50} src="/logo-black.svg" alt="logo" />
         </Link>
+
+        <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
